12113fe03a802d60c9e962291bb7269a6ffd73eb
HIMANSHU JHA <jha.himanshu411@gmail.com>
2022-03-30 14:16:41 +0530

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
+		System.out.println("The multiplication of 64 with 4 is:"+(64*4));	
 	}
 }
